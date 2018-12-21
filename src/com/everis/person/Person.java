7d8aed7d1c5d87e61d5b9236b7635d693db3a959
rrojasli <rrojasli@everis.com>
2018-12-21 13:57:11 -0500

    creacion principal y atributos

--- src/com/everis/person/Person.java
+++ src/com/everis/person/Person.java
@@ -1,5 +1,14 @@
 package com.everis.person;
 
 public class Person {
+	String idpersona;
 
+	public String getIdpersona() {
+		return idpersona;
+	}
+
+	public void setIdpersona(String idpersona) {
+		this.idpersona = idpersona;
+	}
+	
 }
---
+++ src/com/everis/person/Principal.java
@@ -0,0 +1,13 @@
+package com.everis.person;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person persona = new Person();
+		System.out.println("Id Persona: ");
+		persona.setIdpersona("000001");
+		System.out.println(persona.getIdpersona());
+	}
+
+}
